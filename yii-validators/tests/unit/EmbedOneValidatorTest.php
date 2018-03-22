2a5e2c6ac1059a7c58585f00f38706fca6d98a47
Jin Chen <jin.chen@supernovacompanies.biz>
2018-03-22 17:12:58 +0800

    Add test

---
+++ yii-validators/tests/unit/EmbedOneValidatorTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace mole\yii\test\validators;
+
+class EmbedOneValidatorTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \mole\yii\test\validators\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
